826785f382146a368b5a5ff0a3935cbe61968b02
Eric Krukowski <erickruk@amazon.com>
2023-09-05 03:10:54 -0400

    Lab complete

--- server/app.py
+++ server/app.py
@@ -19,5 +19,40 @@ db.init_app(app)
 def index():
     return "Index for Game/Review/User API"
 
+@app.route('/games')
+def games():
+    games=[]
+    for game in Game.query.all():
+        game_dict = create_game_dict(game)
+        games.append(game_dict)
+
+    response = make_response(
+        jsonify(games),
+        200,
+        {"Content-Type": "application/json"} # Default return is text/html, however jsonify configures the response to json anyway so this isn't required in this case
+    )
+    return response
+
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    game_dict = game.to_dict()
+
+    response = make_response(
+        jsonify(game_dict),
+        200
+    )
+    return response
+
+def create_game_dict(game):
+    game_dict = {
+        "title": game.title,
+        "genre": game.genre,
+        "platform": game.platform,
+        "price": game.price
+    }
+
+    return game_dict
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
